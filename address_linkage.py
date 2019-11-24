2a29e9eee13c65be25e091aaece0ab37a37b2e0c
tieming <tiemings@usc.edu>
2019-11-24 11:49:55 -0800

    record linkage part: for authors' university and address

--- address_linkage.py
+++ address_linkage.py
@@ -6,129 +6,156 @@ import recordlinkage as rl
 from recordlinkage.preprocessing import clean
 
 
-def get_ground_truth(df1, df2):
-    df1.columns = df1.columns + '_before'
+class EmailChecking:
 
-    df_combine = df1.merge(df2, how='left', left_on='Email Address_before', right_on='Email Address')
-    df_combine.duplicated()
-    ground_truth = []
-    df_aff = df_combine['Affiliation 1 Department, Institution']
+    def __init__(self):
+        self.email_tags = ['Email Address']
 
-    for i in range(len(df_aff)):
-        j = i + 1
-        while j <= len(df_aff) - 1:
-            if df_aff[i] == df_aff[j]:
-                ground_truth.append((i, j))
-            j = j + 1
-
-    return ground_truth
+    def transform(self, df_dirty):
+        email = df_dirty[self.email_tag].duplicated(keep=False)
+        show_duplicated = df_dirty[email]
+        return show_duplicated
 
 
-def ecm_classifier(index, truth):
-    ecm = rl.ECMClassifier(binarize=0.67)
-    result_ecm = ecm.fit_predict(index)
-    a = rl.precision(truth, result_ecm)
-    b = rl.recall(truth, result_ecm)
-    print("precision = ", a)
-    print("recall = ", b)
-    return result_ecm
+class AddressLinkage:
+
+    def __init__(self):
+        self.output_clean_csv_filename = "cleaned.csv"
+        self.affiliation_tags = [
+            'Affiliation 1 Department, Institution',
+            'Affiliation 2 Department, Institution',
+            'Affiliation 3 Department, Institution'
+        ]
+        self.city_tag = ['City']
+        self.country_tag = ['Country']
+        self.zip_code_tag = ['ZIP']
+        self.state_province_tag = ['State']
+        self.street_tag = ['Street']
+
+    def combine_multi_affiliation(self, df1):
 
 
-def get_cluster(tuple_list):
-    cluster = []
-    for i in tuple_list:
-        flag = 0
-        for j in range(len(cluster)):
-            if i[0] in cluster[j]:
-                if i[1] not in cluster[j]:
-                    cluster[j] = cluster[j] + (i[1],)
-                flag = 1
-                break
-            if i[1] in cluster[j]:
-                if i[0] not in cluster[j]:
-                    cluster[j] = cluster[j] + (i[0],)
-                flag = 1
-                break
-        if flag == 0:
-            cluster.append(i)
-    return cluster
+        return
 
+    def cleaning(self, df_dirty):
+        affiliation = df_dirty[self.affiliation_tags[0]]
+        street = df_dirty[self.street_tag]
+        city = df_dirty[self.city_tag]
+        zip = df_dirty[self.zip_code_tag]
+        state = df_dirty[self.state_province_tag]
+        country = df_dirty[self.country_tag]
 
-def get_longest_affli(tuple_list, df):
-    df['reference_department'] = None
-    for i in range(len(tuple_list)):
-        list1 = []
-        for j in range(len(tuple_list[i])):
-            sort_list = []
-            list1.append(df['department'][tuple_list[i][j]])
+        df_deparments_and_address = df_dirty.iloc[:, [affiliation, street, city, zip, state, country]]
+        df_cleaned0 = df_deparments_and_address.apply(clean, axis=1)
+        df_cleaned = pd.concat([df_dirty['Email Address'], df_cleaned0], axis=1)
 
-        sort_list = [len(one) for one in list1]
-        max_index = tuple_list[i][sort_list.index(max(sort_list))]
-        for j in tuple_list[i]:
-            longest_str = df['department'][max_index]
-            df1 = df.set_value(j, 'reference_department', longest_str)
-    for i in range(len(df1['reference_department'])):
-        if df1['reference_department'][i] == None:
-            df1['reference_department'][i] = df1['department'][i]
-    return df1
+        return df_cleaned
 
+    def get_ground_truth(self, df1, df2):
+        df1.columns = df1.columns + '_before'
 
-# This is the main function
-def address_linkage(df1, df2):
-    # call get_ground_truth
+        df_combine = df1.merge(df2, how='left', left_on='Email Address_before', right_on='Email Address')
+        df_combine.duplicated()
+        ground_truth = []
+        df_aff = df_combine['Affiliation 1 Department, Institution']
 
-    ground_truth = get_ground_truth(df1=df1, df2=df2)
+        for i in range(len(df_aff)):
+            j = i + 1
+            while j <= len(df_aff) - 1:
+                if df_aff[i] == df_aff[j]:
+                    ground_truth.append((i, j))
+                j = j + 1
 
-    new_ground_truth = [(t[1], t[0]) for t in ground_truth]
-    ground_multi = pd.MultiIndex.from_tuples(new_ground_truth)
+        return ground_truth
 
-    # rename columns
-    df1.columns = ['domain name', 'department', 'street', 'city', 'zip', 'state', 'country']
+    def ecm_classifier(self, index, truth):
+        ecm = rl.ECMClassifier(binarize=0.67)
+        result_ecm = ecm.fit_predict(index)
+        a = rl.precision(truth, result_ecm)
+        b = rl.recall(truth, result_ecm)
+        print("precision = ", a)
+        print("recall = ", b)
+        return result_ecm
 
-    # index and check
+    def get_cluster(self, tuple_list):
+        cluster = []
+        for i in tuple_list:
+            flag = 0
+            for j in range(len(cluster)):
+                if i[0] in cluster[j]:
+                    if i[1] not in cluster[j]:
+                        cluster[j] = cluster[j] + (i[1],)
+                    flag = 1
+                    break
+                if i[1] in cluster[j]:
+                    if i[0] not in cluster[j]:
+                        cluster[j] = cluster[j] + (i[0],)
+                    flag = 1
+                    break
+            if flag == 0:
+                cluster.append(i)
+        return cluster
 
-    column_names = ['street', 'city', 'zip', 'country']
-    df_cleaned2 = df1
-    df1 = df1[['street', 'city', 'zip', 'country']]
-    df_cleaned1 = df1.sort_values(by=['country', 'city'])
-    indexer = rl.Index()
+    def get_longest_affli(self, tuple_list, df):
+        df['Reference_department'] = None
+        for i in range(len(tuple_list)):
+            list1 = []
+            for j in range(len(tuple_list[i])):
+                sort_list = []
+                list1.append(df['Department'][tuple_list[i][j]])
 
-    indexer.block(left_on=['country', 'city'])
-    c = indexer.index(df_cleaned1)
+            sort_list = [len(one) for one in list1]
+            max_index = tuple_list[i][sort_list.index(max(sort_list))]
+            for j in tuple_list[i]:
+                longest_str = df['Department'][max_index]
+                df1 = df.set_value(j, 'Reference_department', longest_str)
+        for i in range(len(df1['Reference_department'])):
+            if df1['Reference_department'][i] == None:
+                df1['Reference_department'][i] = df1['Department'][i]
+        return df1
 
-    # compare record
+    # This is the main function, df1 is the open_refined csv file and df2 is the reference csv file.
+    def address_linkage(self, df1, df2):
+        # call get_ground_truth
+        df_cleaned = cleaning(df1)
+        ground_truth = get_ground_truth(df1=df_cleaned, df2=df2)
 
-    compare_cl = rl.Compare()
-    for column_name in column_names:
-        compare_cl.string(column_name, column_name, 'jarowinkler')
-    pair_scoring = compare_cl.compute(c, df1, df1)
-    pair_scoring_light = pair_scoring.iloc[:, [0, 2]]
+        new_ground_truth = [(t[1], t[0]) for t in ground_truth]
+        ground_multi = pd.MultiIndex.from_tuples(new_ground_truth)
 
-    # use ecm_classifier
+        # rename columns
+        # df1.columns = ['domain name', 'department', 'street', 'city', 'zip', 'state', 'country']
 
-    result_ecm = ecm_classifier(pair_scoring_light, ground_multi)
+        # index and check
 
-    # gather the same affiliation together
+        column_names = ['Street', 'City', 'Zip', 'Country']
+        df_cleaned2 = df_cleaned
+        df_cleaned = df_cleaned[['Street', 'City', 'Zip', 'Country']]
+        df_cleaned1 = df_cleaned.sort_values(by=['Country', 'City'])
+        indexer = rl.Index()
 
-    cluster = get_cluster(result_ecm)
-    reference_cluster = get_cluster(ground_multi)
+        indexer.block(left_on=['Country', 'City'])
+        c = indexer.index(df_cleaned1)
 
-    # get the longest affiliation if ecm classifier tests them as the same departments.
+        # compare record
 
-    df_address_reference_long = get_longest_affli(cluster, df_cleaned2)
-    df_address_reference = df_address_reference_long[['department','reference_department']]
-    return df_address_reference
+        compare_cl = rl.Compare()
+        for column_name in column_names:
+            compare_cl.string(column_name, column_name, 'jarowinkler')
+        pair_scoring = compare_cl.compute(c, df1, df1)
+        pair_scoring_light = pair_scoring.iloc[:, [0, 2]]
 
+        # use ecm_classifier
 
-# call function
+        result_ecm = ecm_classifier(pair_scoring_light, ground_multi)
 
-df1 = pd.read_csv("authors-csv-refine.csv")
-df_reference = pd.read_csv("reference.csv")
+        # gather the same affiliation together
 
-df_deparments_and_address = df1.iloc[:, [7, 8, 9, 10, 11, 12]]
+        cluster = get_cluster(result_ecm)
+        # reference_cluster = get_cluster(ground_multi)
 
-df_cleaned0 = df_deparments_and_address.apply(clean, axis=1)
-df_cleaned = pd.concat([df1['Email Address'],df_cleaned0],axis=1)
-df_address_reference = address_linkage(df1=df_cleaned, df2=df_reference)
+        # get the longest affiliation if ecm classifier tests them as the same departments.
 
-
+        df_address_reference_long = get_longest_affli(cluster, df_cleaned2)
+        df_address_reference = df_address_reference_long[['Department', 'Reference_department']]
+        return df_address_reference
