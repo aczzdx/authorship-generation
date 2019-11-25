5f67c770ec8712da2be85bdf1b30928bb17d1e4c
Xingyu Wei <xingyuwe@usc.edu>
2019-11-25 15:18:56 -0800

    update UI

--- utils.py
+++ utils.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import numpy as np
 
-def combine_address(row):
-    tags = ['Affiliation 1 Department, Institution', 'City (e.g. Brisbane)', 'State', 'Country']
-    elements = [row[tag] for tag in tags if type(row[tag]) is str]
+class AddressCombinator:
 
-    return ", ".join(elements)
+    def __init__(self):
+        self.affiliation = 'Affiliation 1 Department, Institution'
+        self.city = 'City (e.g. Brisbane)'
+        self.state = 'State'
+        self.country = 'Country'
+
+    def combine_address(self, row):
+        tags = [self.affiliation, self.city, self.state, self.country]
+        elements = [row[tag] for tag in tags if type(row[tag]) is str]
+        return ", ".join(elements)
